0680db14594a7fea2f14367d515278651ed63c54
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2022-06-09 14:43:47 +0200

    timer

--- Assets/Scripts/BulletShot.cs
+++ Assets/Scripts/BulletShot.cs
@@ -6,16 +6,21 @@ using UnityEngine;
 public class BulletShot : MonoBehaviour
 {
 	[SerializeField] float bulletLife;
+	[SerializeField] string score;
+	
 	void Awake()
 	{
 		Destroy(gameObject,3);
+		score = 0;
 	}
-
 
     private void OnCollisionEnter(Collision collision) {
         
         if(collision.gameObject.tag == "target")
             Destroy(collision.gameObject);
+            score = score++;
+            Tostring(score);
+
         
         Destroy(gameObject);
     }
