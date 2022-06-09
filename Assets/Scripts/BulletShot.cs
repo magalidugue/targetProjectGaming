c1792ad22d4da5a55b148ff815040515fac458db
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2022-06-09 10:25:43 +0200

    shoot

--- Assets/Scripts/BulletShot.cs
+++ Assets/Scripts/BulletShot.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BulletShot : MonoBehaviour
 {
+	[SerializeField] float bulletLife;
+	void Awake()
+	{
+		Destroy(gameObject,3);
+	}
+
+
     private void OnCollisionEnter(Collision collision) {
-        if(collision.gameObject.tag == "target"){
-            Destroy(gameObject);
-        }
+        
+        if(collision.gameObject.tag == "target")
+            Destroy(collision.gameObject);
+        
+        Destroy(gameObject);
     }
 }
